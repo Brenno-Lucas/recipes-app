dd865d4c1229ac5b7bd5e00ce03e57bb3fbb01c9
Udison Abreu <udi0abreu@gmail.com>
2022-09-23 16:45:33 -0300

    requisitos 2 e 3 - feitos

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <main>
+      <form>
+        <input type="text" data-testid="email-input" />
+        <input type="password" data-testid="password-input" />
+        <button type="button" data-testid="login-submit-btn">Enter</button>
+      </form>
+    </main>
+  );
 }
