2275e53bcb15739bbde3379eeeff5238ed05993f
caiocsper <ccpereira0498@gmail.com>
2022-09-26 20:06:50 -0300

    Retirando lixo de código

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,44 +1,14 @@
 import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import {
-  requestDrinks,
-  requestDrinksByCategory,
-  requestMeals,
-  requestMealsByCategory,
-} from '../redux/actions';
 import { fetchMealsCategories } from '../services/mealsApi';
 import { fetchDrinksCategories } from '../services/drinksApi';
 import { MAX_CATEGORIES_QUANTITY } from '../utils/constants';
 import CategoryCard from './CategoryCard';
 
-function Categories(props) {
-  const {
-    fetchMealsByCategory,
-    fetchDrinksByCategory,
-    fetchAllMeals,
-    fetchAllDrinks,
-  } = props;
+export default function Categories() {
   const [categories, setCategories] = useState([]);
   const [activeFilter, setActiveFilter] = useState('All');
   const { location: { pathname } } = useHistory();
-
-  const handleCategoryFilter = (categoryName) => {
-    if (pathname === '/meals') {
-      fetchMealsByCategory(categoryName);
-    } else {
-      fetchDrinksByCategory(categoryName);
-    }
-  };
-
-  const handleAllCategoryFilter = () => {
-    if (pathname === '/meals') {
-      fetchAllMeals();
-    } else {
-      fetchAllDrinks();
-    }
-  };
 
   useEffect(() => {
     const requestCategories = async () => {
@@ -57,7 +27,6 @@ function Categories(props) {
       <CategoryCard
         activeFilter={ activeFilter }
         setActiveFilter={ setActiveFilter }
-        handleFilter={ handleAllCategoryFilter }
       />
       {
         categories.map(({ strCategory }) => (
@@ -66,26 +35,9 @@ function Categories(props) {
             activeFilter={ activeFilter }
             setActiveFilter={ setActiveFilter }
             categoryName={ strCategory }
-            handleFilter={ handleCategoryFilter }
           />
         ))
       }
     </section>
   );
 }
-
-Categories.propTypes = {
-  fetchMealsByCategory: PropTypes.func.isRequired,
-  fetchDrinksByCategory: PropTypes.func.isRequired,
-  fetchAllMeals: PropTypes.func.isRequired,
-  fetchAllDrinks: PropTypes.func.isRequired,
-};
-
-const mapDispatchToProps = (dispatch) => ({
-  fetchMealsByCategory: (catName) => dispatch(requestMealsByCategory(catName)),
-  fetchDrinksByCategory: (catName) => dispatch(requestDrinksByCategory(catName)),
-  fetchAllMeals: () => dispatch(requestMeals),
-  fetchAllDrinks: () => dispatch(requestDrinks),
-});
-
-export default connect(null, mapDispatchToProps)(Categories);
