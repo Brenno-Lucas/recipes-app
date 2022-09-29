cc41cfbfc6b1212ee6f74cb75c1b5139937e58ee
Udison Abreu <udi0abreu@gmail.com>
2022-09-29 16:12:00 -0300

    testes da página de receitas favoritas

---
+++ src/tests/mocks/localStorageMock.js
@@ -0,0 +1,40 @@
+const favoriteRecipes = [
+  {
+    id: '53060',
+    type: 'meal',
+    nationality: 'Croatian',
+    category: 'Side',
+    alcoholicOrNot: '',
+    name: 'Burek',
+    image: 'https://www.themealdb.com/images/media/meals/tkxquw1628771028.jpg',
+  },
+  {
+    id: '53065',
+    type: 'meal',
+    nationality: 'Japanese',
+    category: 'Seafood',
+    alcoholicOrNot: '',
+    name: 'Sushi',
+    image: 'https://www.themealdb.com/images/media/meals/g046bb1663960946.jpg',
+  },
+  {
+    id: '17225',
+    type: 'drink',
+    nationality: '',
+    category: 'Cocktail',
+    alcoholicOrNot: 'Alcoholic',
+    name: 'Ace',
+    image: 'https://www.thecocktaildb.com/images/media/drink/l3cd7f1504818306.jpg',
+  },
+  {
+    id: '17837',
+    type: 'drink',
+    nationality: '',
+    category: 'Ordinary Drink',
+    alcoholicOrNot: 'Alcoholic',
+    name: 'Adam',
+    image: 'https://www.thecocktaildb.com/images/media/drink/v0at4i1582478473.jpg',
+  },
+];
+
+export default favoriteRecipes;
