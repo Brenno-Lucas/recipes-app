ab3acfbf790235d8e9242ac35264464fb50c0991
Brenno-Lucas <brennolucas8@gmail.com>
2022-09-29 19:50:04 -0300

    Requisitos 37-39(exceto testes)

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Recipes from './pages/Recipes';
 import MealRecipe from './components/MealRecipe';
 import DrinkRecipe from './components/DrinkRecipe';
 import FavoriteRecipes from './pages/FavoriteRecipes';
-import recipesInProgress from './pages/Recipesinprogress';
+import RecipesInProgress from './pages/Recipesinprogress';
 
 function App() {
   return (
@@ -20,8 +20,8 @@ function App() {
         <Route exact path="/drinks" component={ Recipes } />
         <Route exact path="/meals/:id" component={ MealRecipe } />
         <Route exact path="/drinks/:id" component={ DrinkRecipe } />
-        <Route exact path="/meals/:id/in-progress" component={ recipesInProgress } />
-        <Route exact path="/drinks/:id/in-progress" component={ recipesInProgress } />
+        <Route exact path="/meals/:id/in-progress" component={ RecipesInProgress } />
+        <Route exact path="/drinks/:id/in-progress" component={ RecipesInProgress } />
         <Route exact path="/profile" component={ Profile } />
         <Route exact path="/done-recipes" component={ DoneRecipes } />
         <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
--- src/pages/Recipesinprogress.jsx
+++ src/pages/Recipesinprogress.jsx
@@ -1,9 +1,93 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+import FavoriteButton from '../components/FavoriteButton';
+import ShareButton from '../components/ShareButton';
+import { fetchMealsById } from '../services/mealsApi';
+import { fetchDrinksById } from '../services/drinksApi';
 
-export default function recipesInProgress() {
+export default function RecipesInProgress({ match }) {
+  const { id } = match.params;
+  const { location: { pathname } } = useHistory();
+  const recipeID = pathname.split('/');
+  const [recipe, setRecipe] = useState({});
+  const [listOfIngredients, setListOfIngredients] = useState([]);
+  useEffect(() => {
+    const setMeals = async () => {
+      setRecipe(await fetchMealsById(id));
+    };
+    const setDrinks = async () => {
+      setRecipe(await fetchDrinksById(id));
+    };
+    switch (window.location.href.includes('meals')) {
+    case true:
+      setMeals();
+      break;
+    default:
+      setDrinks();
+      break;
+    }
+  }, [id]);
+
+  useEffect(() => {
+    const MAX_NUMBER_OF_INGREDIENTS = 20;
+    let auxIngredients = [];
+    for (let index = 1; index <= MAX_NUMBER_OF_INGREDIENTS; index += 1) {
+      const ingredient = `strIngredient${index}`;
+      const quantity = `strMeasure${index}`;
+
+      auxIngredients = [...auxIngredients,
+        {
+          ingredient: recipe[ingredient],
+          quantity: recipe[quantity],
+        }];
+    }
+    auxIngredients = auxIngredients.filter(({ ingredient }) => ingredient !== ''
+      && ingredient);
+    setListOfIngredients([...auxIngredients]);
+  }, [recipe]);
+
   return (
     <div>
-      Recipes in progress
+      <img
+        src={ recipeID[1] === 'meals' ? recipe.strMealThumb : recipe.strDrinkThumb }
+        alt="recipe"
+        data-testid="recipe-photo"
+      />
+      <p
+        data-testid="recipe-title"
+      >
+        { recipeID[1] === 'meals' ? recipe.strMeals : recipe.strDrink }
+      </p>
+      <FavoriteButton />
+      <ShareButton />
+      <p data-testid="recipe-category">{recipe.strCategory}</p>
+      <p data-testid="instructions">{recipe.strInstructions}</p>
+      <button type="button" data-testid="finish-recipe-btn">Finish Recipe</button>
+      <section>
+        {
+          listOfIngredients.map(({ ingredient, quantity }, index) => (
+            <label
+              key={ index }
+              data-testid={ `${index}-ingredient-step` }
+              htmlFor="check"
+            >
+              {ingredient}
+              -
+              {quantity}
+              <input type="checkbox" name="" id="check" />
+            </label>
+          ))
+        }
+      </section>
     </div>
   );
 }
+
+RecipesInProgress.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
