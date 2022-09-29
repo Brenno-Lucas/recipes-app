b7d31670f3d8d979abff9f2e9134ad86c1a0cd30
Brenno-Lucas <brennolucas8@gmail.com>
2022-09-29 15:55:19 -0300

    Iniciando Tela de Receitas em Progresso

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Recipes from './pages/Recipes';
 import MealRecipe from './components/MealRecipe';
 import DrinkRecipe from './components/DrinkRecipe';
 import favoriteRecipes from './pages/FavoriteRecipes';
+import recipesInProgress from './pages/Recipesinprogress';
 
 function App() {
   return (
@@ -19,8 +20,8 @@ function App() {
         <Route exact path="/drinks" component={ Recipes } />
         <Route exact path="/meals/:id" component={ MealRecipe } />
         <Route exact path="/drinks/:id" component={ DrinkRecipe } />
-        {/* <Route exact path="/meals/:id/in-progress" component={  } />
-        <Route exact path="/drinks/:id/in-progress" component={  } /> */}
+        <Route exact path="/meals/:id/in-progress" component={ recipesInProgress } />
+        <Route exact path="/drinks/:id/in-progress" component={ recipesInProgress } />
         <Route exact path="/profile" component={ Profile } />
         <Route exact path="/done-recipes" component={ DoneRecipes } />
         <Route exact path="/favorite-recipes" component={ favoriteRecipes } />
---
+++ src/pages/Recipesinprogress.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function recipesInProgress() {
+  return (
+    <div>
+      Recipes in progress
+    </div>
+  );
+}
