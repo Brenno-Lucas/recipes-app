d7383da51a4a308ef98936fc58ee2a9d373d8245
LeandroNani <leandropacheco02@hotmail.com>
2022-09-29 15:26:29 -0300

    testes corrigidos

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ function App() {
         <Route exact path="/" component={ Login } />
         <Route exact path="/meals" component={ Recipes } />
         <Route exact path="/drinks" component={ Recipes } />
-        {/* <Route exact path="/meals/:meal-id" component={  } /> */}
-        {/* <Route exact path="/drinks/:drink-id" component={  } /> */}
+        <Route exact path="/meals/:id" component={ MealRecipe } />
+        <Route exact path="/drinks/:id" component={ DrinkRecipe } />
         {/* <Route exact path="/meals/:meal-id/in-progress" component={ MealInProgress } /> */}
         {/* <Route exact path="/drinks/:drink-id/in-progress" component={ DrinkInProgress } /> */}
         <Route exact path="/profile" component={ Profile } />
